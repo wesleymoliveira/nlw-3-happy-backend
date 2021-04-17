4cb6d6bec8e58e063df5a74fedfb1beb5ff2c4ec
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-04-17 15:06:50 -0300

    general adjusts after package update

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -1,36 +1,38 @@
-import { Request, Response} from 'express';
-import jwt from 'jsonwebtoken';
-import { getRepository } from 'typeorm';
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { getRepository } from "typeorm";
 
-import * as Yup from 'yup';
-import User from '../models/Users';
-import authConfig from '../config/auth';
+import * as Yup from "yup";
+import User from "../models/Users";
+import authConfig from "../config/auth";
 
-export default{
-
+export default {
   async store(req: Request, res: Response) {
-
     const schema = Yup.object().shape({
       email: Yup.string().email().required(),
       password: Yup.string().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Opa :(  Você inseriu alguma informação errada.' });
+      return res
+        .status(400)
+        .json({ error: "Opa :(  Você inseriu alguma informação errada." });
     }
 
     const { email, password } = req.body;
 
     const usersRepository = getRepository(User);
 
-     const user = await usersRepository.findOne({ where: { email: req.body.email } });
+    const user = await usersRepository.findOne({
+      where: { email: req.body.email },
+    });
 
     if (!user) {
-      return res.status(401).json({ error: 'Usuário não encontrado' });
+      return res.status(401).json({ error: "Usuário não encontrado" });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Senha incorreta' });
+      return res.status(401).json({ error: "Senha incorreta" });
     }
 
     const { id, name } = user;
@@ -45,5 +47,5 @@ export default{
         expiresIn: authConfig.expiresIn,
       }),
     });
-  }
-}
+  },
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,26 +1,25 @@
-import { Router } from 'express';
-import multer from 'multer';
+import { Router } from "express";
+import multer from "multer";
 
-import UserController from './controllers/UsersController';
-import SessionController from './controllers/SessionController';
+import UserController from "./controllers/UsersController";
+import SessionController from "./controllers/SessionController";
 
-import uploadConfig from './config/upload';
-import OrphanagesController from './controllers/OrphanagesController';
+import uploadConfig from "./config/upload";
+import OrphanagesController from "./controllers/OrphanagesController";
 
-import authMiddleware from './middlewares/auth';
+import authMiddleware from "./middlewares/auth";
 
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.post('/users', UserController.create);
+routes.post("/users", UserController.create);
+routes.post("/sessions", SessionController.store);
 
-routes.post('/sessions', SessionController.store);
+routes.use(authMiddleware);
+routes.get("/orphanages", OrphanagesController.index);
+routes.get("/orphanages/:id", OrphanagesController.show);
+routes.post("/orphanages", upload.array("images"), OrphanagesController.create);
 
-routes.get('/orphanages', OrphanagesController.index);
-routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
-
-routes.use(authMiddleware); 
-routes.get('/users', UserController.index);
+routes.get("/users", UserController.index);
 
 export default routes;
