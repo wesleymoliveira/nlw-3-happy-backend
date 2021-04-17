0057703f8549759337809740693dc0211569dfe2
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-04-17 14:30:01 -0300

    fix auth middleware position

--- src/routes.ts
+++ src/routes.ts
@@ -7,11 +7,7 @@ import UserController from "./controllers/UsersController";
 import uploadConfig from "./config/upload";
 import OrphanagesController from "./controllers/OrphanagesController";
 
-<<<<<<< HEAD
 import authMiddleware from "./middlewares/auth";
-=======
-//import authMiddleware from "./middlewares/auth";
->>>>>>> 2fc391710cfa4fbb4a1952cca38c7cf69fa61555
 
 const routes = Router();
 const upload = multer(uploadConfig);
@@ -19,11 +15,7 @@ const upload = multer(uploadConfig);
 routes.post("/sessions", SessionController.store);
 routes.post("/users", UserController.create);
 
-<<<<<<< HEAD
 routes.use(authMiddleware);
-=======
-//routes.use(authMiddleware);
->>>>>>> 2fc391710cfa4fbb4a1952cca38c7cf69fa61555
 routes.get("/orphanages", OrphanagesController.index);
 routes.get("/orphanages/:id", OrphanagesController.show);
 routes.post("/orphanages", upload.array("images"), OrphanagesController.create);
