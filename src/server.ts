23e0d6d2b08205d893d2d4608495f9291b87c49d
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-04-18 11:23:39 -0300

    Deploy config on new api project

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import "reflect-metadata";
-import "dotenv/config";
 import path from "path";
+
+import "dotenv/config";
+import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+
 import "./database/connection";
 import routes from "./routes";
 import errorHandler from "./errors/handler";
