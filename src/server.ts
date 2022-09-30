6fd5732d2d48b39c25479f1b18e785a2baa83f47
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2022-09-29 21:44:40 -0300

    fix cors errors

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,18 @@ import errorHandler from "./errors/handler";
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use(routes);
